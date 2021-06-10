1094bd5693df59ba4a222fdb89b01b87b2e29687
orlando taylor <ackerie_taylor@hotmail.com>
2021-06-09 20:26:31 -0400

    push

---
+++ index.js
@@ -0,0 +1,21 @@
+const dotenv = require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 9000
+
+app.use(cors())
+app.use('/api', (_,res)=>{
+    res.json({data:'The API is serving data!'})
+})
+
+
+app.listen(port,()=>{
+    console.log(`Server is alive on port ${port}`)
+})
+console.log('its alive')
+console.log(__dirname)
+console.log(__filename)
+console.log(process.env.USERNAME)
+console.log(process.env.PORT)
+
