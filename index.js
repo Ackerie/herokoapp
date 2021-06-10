f77f81ee355d02aa8fc6bc35605f2a9380cab48a
orlando taylor <ackerie_taylor@hotmail.com>
2021-06-09 21:18:26 -0400

    mono push

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 9000
+const path = require('path')
+
 
 app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname,"client/build")))
+
 app.use('/api', (_,res)=>{
     res.json({data:'The API is serving data!'})
 })
